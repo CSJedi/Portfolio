366ae8b4d3d3db692f06698e35e53b7c85cb8d9c
tetiana.zhydeikina <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-02-06 13:34:50 -0800

    site with first model photo

---
+++ portfolio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django import forms
+from django.utils import timezone
+
+class ImageUpload(forms.Form):
+    image = forms.ImageField()
+
+class Photo(models.Model):
+    model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no_image_300x300.jpg')
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    
+
+    '''def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title'''
